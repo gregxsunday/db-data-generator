320b319e20318815ed4155198f2a6c8efe23c844
unknown <gregxsunday@gmail.com>
2019-01-16 07:45:49 +0100

    added costDiscounts generator

--- generate.py
+++ generate.py
@@ -1,23 +1,29 @@
 import json
 from dateutil import rrule
+from dateutil.relativedelta import relativedelta
 from datetime import datetime, timedelta
 from random import randint
 
-'''
-72 konferencje
-72 nazwy konferencji
-daty - równy podział trzech lat na 72
-czas trwania - 36 2-dniowych i 36 3-dniowych
-cost = rand(100, 1000)
 
-
-'''
 class Conference:
-    def __init__(self, name):
+    def __init__(self, id, name, date_begin, date_end, cost, max_participants, student_discount):
+        self.id = id
         self.name = name + ' Conference'
+        self.date_begin = date_begin
+        self.date_end = date_end
+        self.cost = cost
+        self.max_participants = max_participants
+        self.student_discount = student_discount
 
-if __name__ == "__main__":
-    #Conferences
+class Discount:
+    def __init__(self, conf_id, discount, date_begin, date_end):
+        self.conf_id = conf_id
+        self.discount = discount
+        self.date_begin = date_begin
+        self.date_end = date_end
+
+def generate_conferences():
+    conferences = []
     with open('conf_names', 'r') as infile:
         conference_names = infile.read().split('\n')
 
@@ -25,7 +31,6 @@ if __name__ == "__main__":
     date_max = datetime(2021, 12, 31)
     dates_start = list(rrule.rrule(rrule.WEEKLY, dtstart=date_zero, until=date_max, interval=2))
 
-    print(len(dates_start), len(conference_names))
     for i in range(len(dates_start)):
         conf_id = i
         conf_name = conference_names[i]
@@ -34,4 +39,26 @@ if __name__ == "__main__":
         cost = randint(10, 100)*10
         max_participants = randint(10, 30)*10
         studentDiscount = round(randint(5, 8)*0.1, 1)
-        print(conf_id, conf_name, date_begin, date_end, cost, max_participants, studentDiscount, sep=';')
+        conferences.append(Conference(conf_id, conf_name, date_begin, date_end, cost, max_participants, studentDiscount))
+    return conferences
+
+def generate_discounts(conference):
+    discounts = []
+    months_before = randint(3, 6)
+    discount_jump = round(randint(5, 10)*.01, 2)
+    cr_discount = 1
+    cr_date_end = conference.date_begin
+    for i in range(months_before):
+        conf_id = conference.id
+        discount = cr_discount
+        cr_discount -= discount_jump
+        date_end = cr_date_end
+        cr_date_end -= timedelta(days=30)
+        date_begin = cr_date_end
+        discounts.append(Discount(conf_id, round(discount, 2), date_begin, date_end))
+    return discounts
+
+if __name__ == "__main__":
+    #Conferences
+    confs = generate_conferences()
+    
