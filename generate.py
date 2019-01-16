536606b7403950475b6048b5497982161e69ecd2
unknown <gregxsunday@gmail.com>
2019-01-16 06:14:47 +0100

    init

---
+++ generate.py
@@ -0,0 +1,17 @@
+import json
+'''
+72 konferencje
+72 nazwy konferencji
+daty - równy podział trzech lat na 72
+czas trwania - 36 2-dniowych i 36 3-dniowych
+cost = rand(100, 1000)
+
+
+'''
+class Conference():
+    def Conference(self, name):
+        self.name = name + ' Conference'
+        self.
+
+if __name__ == "__main__":
+    print()
