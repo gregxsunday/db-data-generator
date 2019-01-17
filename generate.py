c04521afa2238f11cc10bd1bd96bf103e2f2d48b
unknown <gregxsunday@gmail.com>
2019-01-17 07:59:04 +0100

    conferences with discounts

--- generate.py
+++ generate.py
@@ -6,8 +6,9 @@ from random import randint
 
 
 class Conference:
-    def __init__(self, id, name, date_begin, date_end, cost, max_participants, student_discount):
+    def __init__(self, id, addr_id, name, date_begin, date_end, cost, max_participants, student_discount):
         self.id = id
+        self.addr_id = addr_id
         self.name = name + ' Conference'
         self.date_begin = date_begin
         self.date_end = date_end
@@ -16,14 +17,18 @@ class Conference:
         self.student_discount = student_discount
 
     def __str__(self):
-        return '(' + str(self.id) + ', ' + '\'' + self.name + '\', ' + 'CAST(\'' + str(self.date_begin) + '\' AS smalldatetime), CAST(\'' + str(self.date_end) + '\' AS smalldatetime), ' + str(self.cost) + ', ' + str(self.max_participants) + ', ' + str(self.student_discount) + '),'
+        return '(' + str(self.id) + ', ' + str(self.addr_id) + ', \'' + self.name + '\', ' + 'CAST(\'' + str(self.date_begin) + '\' AS smalldatetime), CAST(\'' + str(self.date_end) + '\' AS smalldatetime), ' + str(self.cost) + ', ' + str(self.max_participants) + ', ' + str(self.student_discount) + '),'
 
 class Discount:
-    def __init__(self, conf_id, discount, date_begin, date_end):
+    def __init__(self, id, conf_id, discount, date_begin, date_end):
+        self.id = id
         self.conf_id = conf_id
         self.discount = discount
         self.date_begin = date_begin
         self.date_end = date_end
+
+    def __str__(self):
+        return '( ' + str(self.id) + ', ' + str(self.conf_id) + ', ' + str(self.discount) + ', CAST(\'' + str(self.date_begin) + '\' AS smalldatetime), CAST(\'' + str(self.date_end) + '\' AS smalldatetime)),'
 
 class workshop_info:
     def __init__(self, id, name, max_parts):
@@ -60,10 +65,11 @@ def generate_conferences():
         cost = randint(10, 100)*10
         max_participants = randint(10, 30)*10
         studentDiscount = round(randint(5, 8)*0.1, 1)
-        conferences.append(Conference(conf_id, conf_name, date_begin, date_end, cost, max_participants, studentDiscount))
+        addr_id = randint(0, 39)
+        conferences.append(Conference(conf_id, addr_id, conf_name, date_begin, date_end, cost, max_participants, studentDiscount))
     return conferences
 
-def generate_discounts(conference):
+def generate_discounts(id, conference):
     discounts = []
     months_before = randint(3, 6)
     discount_jump = round(randint(5, 10)*.01, 2)
@@ -76,8 +82,9 @@ def generate_discounts(conference):
         date_end = cr_date_end
         cr_date_end -= timedelta(days=30)
         date_begin = cr_date_end
-        discounts.append(Discount(conf_id, round(discount, 2), date_begin, date_end))
-    return discounts
+        discounts.append(Discount(id, conf_id, round(discount, 2), date_begin, date_end))
+        id += 1
+    return discounts, id
 
 def generate_workshop_info(id, conf_name):
     workshops = []
@@ -100,6 +107,14 @@ def generate_workshop_info(id, conf_name):
 if __name__ == "__main__":
     #Conferences
     confs = generate_conferences()
+    with open('fill_conferences.sql', 'w') as outfile:
+        disc_id = 0
+        for c in confs:
+            discounts, disc_id = generate_discounts(disc_id, c)
+            outfile.write(str(c) + '\n')
+            with open('fill_discounts.sql', 'a') as out_discounts:
+                for d in discounts:
+                    out_discounts.write(str(d) + '\n')
     # with open('fill_workshopinfo.sql', 'a') as outfile:
     #     i = 0
     #     for conf in confs:
@@ -107,48 +122,4 @@ if __name__ == "__main__":
     #         for w in workshops:
     #             outfile.write(str(w) + '\n')
     #         i += 4
-    streets = '''2559 West Place
-74 Valley Edge Road
-39 Knutson Road
-02 Crownhardt Lane
-999 Blaine Alley
-53412 Londonderry Park
-0505 Esch Alley
-8782 Springview Pass
-725 Dwight Place
-1 Sutherland Court
-98898 Center Pass
-0535 Shopko Avenue
-37723 Grasskamp Circle
-356 Donald Point
-9 Cherokee Avenue
-4 Forest Alley
-706 Helena Pass
-64 Arapahoe Pass
-6406 Reinke Terrace
-8 Debs Alley
-662 Delaware Junction
-427 Roth Terrace
-0 Daystar Point
-43 Westridge Junction
-55 Hermina Junction
-3 Holy Cross Plaza
-39 Vidon Lane
-42155 School Street
-6 Farwell Road
-0 Talmadge Way
-3 Melby Parkway
-25 Bellgrove Center
-1 Hoffman Drive
-5 Messerschmidt Crossing
-9 Esker Avenue
-8 Nevada Park
-20 Oneill Hill
-1 Spohn Center
-639 Daystar Terrace
-3 Independence Terrace'''
-    i = 0
-    for street in streets.split('\n'):
-        print('(' + str(i) + ', ' + str(randint(0, 29)) + ', \'' + street + '\'),')
-        i += 1
 
